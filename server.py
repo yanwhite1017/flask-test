ba0cb6d71576e8bbd3ead2896a5f4ad1fa1b489e
yanny <52163207+yanwhite1017@users.noreply.github.com>
2024-12-08 18:15:52 +0500

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-  return "<h1>get started</h1>"
+  return "<h1>Анюта = солнышко</h1>"
 
 if __name__ == '__main__':
   app.run(debug=True, port=80, host="0.0.0.0")
